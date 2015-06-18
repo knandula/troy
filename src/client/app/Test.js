4de7d1af89f56b9546a84ff5107ed231e2897867
Krishna Nandula <knandula@factset.com>
2015-06-18 14:29:37 +0530

    herokuchecking

--- src/client/app/Test.js
+++ src/client/app/Test.js
@@ -2,4 +2,3 @@
  * Created by rachanti on 5/30/2015.
  */
 
-alert("waste");
--- src/server/app.js
+++ src/server/app.js
@@ -2,40 +2,13 @@
 
 var express = require('express');
 var app = express();
-
-//var bodyParser = require('body-parser');
-//var compress = require('compression');
-//var cors = require('cors');
-//var errorHandler = require('./routes/utils/errorHandler')();
-//var favicon = require('serve-favicon');
-//var logger = require('morgan');
 var port = process.env.PORT || 7203;
-//var routes;
-
 var environment = process.env.NODE_ENV;
 
-//app.use(favicon(__dirname + '/favicon.ico'));
-//app.use(bodyParser.urlencoded({
-//    extended: true
-//}));
-//app.use(bodyParser.json());
-//app.use(compress());
-//app.use(logger('dev'));
-//app.use(cors());
-//app.use(errorHandler.init);
-//
-//routes = require('./routes/index')(app);
-
-//console.log("testing");
 console.log('About to crank up node');
 console.log('PORT=' + port);
 console.log('NODE_ENV=' + environment);
 
-//app.get('/ping', function(req, res, next) {
-//    console.log(req.body);
-//    res.send('pong');
-//});
-//
 switch (environment) {
     case 'build':
         console.log('** BUILD **');
