f28f65bab4a2aab8a6c7eef435ed56ec6be73259
Krishna Nandula <knandula@factset.com>
2015-07-06 17:58:48 +0530

    code modified

--- src/client/app/Controllers/streatbeatController.js
+++ src/client/app/Controllers/streatbeatController.js
@@ -2,56 +2,7 @@
  * Created by rachanti on 6/23/2015.
  */
 app.controller("streatbeatcntrl",['$scope','$http',function($scope,$http){
-    //var geojsonFeature = {
-    //    "type": "Feature",
-    //    "properties": {
-    //        "name": "Coors Field",
-    //        "amenity": "Baseball Stadium",
-    //        "popupContent": "This is where the Rockies play!"
-    //    },
-    //    "geometry": {
-    //        "type": "Point",
-    //        "coordinates": [-104.99404, 39.75621]
-    //    }
-    //};
-    //
-    var states = [{
-        "type": "Feature",
-        "properties": {"party": "Republican"},
-        "geometry": {
-            "type": "Polygon",
-            "coordinates": [[
-                [-104.05, 48.99],
-                [-97.22,  48.98],
-                [-96.58,  45.94],
-                [-104.03, 45.94],
-                [-104.05, 48.99]
-            ]]
-        }
-    }, {
-        "type": "Feature",
-        "properties": {"party": "Democrat"},
-        "geometry": {
-            "type": "Polygon",
-            "coordinates": [[
-                [-109.05, 41.00],
-                [-102.06, 40.99],
-                [-102.03, 36.99],
-                [-109.04, 36.99],
-                [-109.05, 41.00]
-            ]]
-        }
-    }];
-    //
-    //var myLines = [{
-    //    "type": "LineString",
-    //    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
-    //}, {
-    //    "type": "LineString",
-    //    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
-    //}];
-
-    var myStyle = {
+     var myStyle = {
         "color": "#ff7800",
         "weight": 5,
         "opacity": 0.65
@@ -69,13 +20,7 @@ app.controller("streatbeatcntrl",['$scope','$http',function($scope,$http){
         $http.get('http://localhost:3000/api/maplayers').success(function(response){
              $scope.lays= response;
 
-            console.log(response);
-            //method 1
-            //var myLayer = L.geoJson().addTo(map);
-            //myLayer.addData( $scope.lays);
-
-            //method 2
-            L.geoJson($scope.lays,{onEachFeature : onEachFeature}).addTo(map);
+           // L.geoJson($scope.lays,{onEachFeature : onEachFeature}).addTo(map);
 
             console.log("Next");
         }).error(function()
@@ -102,15 +47,13 @@ app.controller("streatbeatcntrl",['$scope','$http',function($scope,$http){
                 "coordinates": [-125.99404, 39.75621]
             }
         };
-        //
-        //console.log( geojsonFeature);
-        //console.log( $scope.lays);  //undefined
 
-        L.geoJson(geojsonFeature).addTo(map);
 
-        //map.setView([51.433333, 5.483333], 12);
-        //L.geoJson(geojsonFeature).addTo(map);
-        //L.geoJson(myLines, {style: myStyle}).addTo(map);
+
+
+
+              L.geoJson(geojsonFeature).addTo(map);
+
         L.geoJson(states, {
             style: function(feature) {
                 switch (feature.properties.party) {
--- src/client/streatbeat.js
+++ src/client/streatbeat.js
@@ -29,8 +29,47 @@ app.directive('mapbox', [
             },
             template: '<div></div>',
             link: function (scope, element, attributes) {
+
+                var currentlats;
+                var currnetlong;
+
                 L.mapbox.accessToken = 'pk.eyJ1IjoiYmx1ZWdlbmUiLCJhIjoiZjMwNzU2ZmQyMzdlMGQ3YjlkYTRmYmY3ZGY5N2RhMDMifQ.gnt0BCmgUCChF56g7kEo7Q';
-                var map = L.mapbox.map(element[0], 'examples.map-i86nkdio');
+
+
+
+                var map = L.mapbox.map(element[0],null);
+
+                var layers = {
+                    SatelliteView : L.mapbox.tileLayer('bluegene.mfl8kdhk'),
+                    StreetView : L.mapbox.tileLayer('bluegene.ffdb711a'),
+                    DarkView : L.mapbox.tileLayer('bluegene.mfl79iea')
+                };
+
+                layers.StreetView.addTo(map);
+                L.control.layers(layers).addTo(map);
+                map.locate();
+
+                var myLayer = L.mapbox.featureLayer().addTo(map);
+
+                map.on('locationfound', function(e) {
+                    currentlats = e.latlng.lat;
+                    currnetlong = e.latlng.lng;
+                    map.fitBounds(e.bounds);
+                    myLayer.setGeoJSON({
+                        type: 'Feature',
+                        geometry: {
+                            type: 'Point',
+                            coordinates: [e.latlng.lng, e.latlng.lat]
+                        },
+                        properties: {
+                            'title': 'StreatBeat',
+                            'marker-color': '#ff8888',
+                            'marker-size': 'small',
+                            'marker-symbol': 'circle'
+                        }
+                    });
+                    map.setView([currentlats,currnetlong],5);
+                });
                 scope.callback(map);
             }
         };
